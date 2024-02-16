b427c606710c39ff65a5b4701b5e490d4a339516
Myster7494 <jackychiu0101@gmail.com>
2024-02-16 21:42:31 +0800

    Initial commit

---
+++ src/main/java/me/myster/pipetech/PipeTechDataGenerator.java
@@ -0,0 +1,11 @@
+package me.myster.pipetech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PipeTechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
